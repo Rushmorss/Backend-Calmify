f560e0841a0f82e96c468a125b4a478011dd2dc5
rushmorss <thangnguyen151004@gmail.com>
2025-12-16 02:27:09 +0700

    Update statisticalController.js

--- src/controllers/statisticalController.js
+++ src/controllers/statisticalController.js
@@ -1,28 +1,34 @@
-import statisticalService  from "../services/statisticalService.js";
+import statisticalService from "../services/statisticalService.js";
 
 const getStats = async (req, res) => {
     try {
-        const userId = req.user.id; 
-        const { type, date } = req.query; 
-        if (!type || !['day', 'month', 'year'].includes(type)) {
-            return res.status(400).json({ 
-                success: false, 
-                message: "Tham số 'type' không hợp lệ (day, month, year)." 
+        const userId = req.user.id;
+        const { type, date } = req.query;
+
+        const validTypes = ['day', 'week', 'month', 'quarter', 'year'];
+
+        if (!type || !validTypes.includes(type)) {
+            return res.status(400).json({
+                success: false,
+                message: "Tham số 'type' không hợp lệ (day, week, month, quarter, year)."
             });
         }
+
         const data = await statisticalService.getStatistics(userId, type, date);
+        
         return res.status(200).json({
             success: true,
             data: data
         });
     } catch (error) {
         console.error("Lỗi thống kê:", error);
-        return res.status(500).json({ 
-            success: false, 
-            message: "Lỗi server." 
+        return res.status(500).json({
+            success: false,
+            message: "Lỗi server."
         });
     }
 };
+
 export default {
     getStats
 };
