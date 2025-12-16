fc4b19bf481123dad5d5507e726a52dc0ea2ab73
rushmorss <thangnguyen151004@gmail.com>
2025-12-16 16:37:37 +0700

    file authMiddleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,28 +6,41 @@ const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key";
 
 const authMiddleware = async (req, res, next) => {
   try {
+    let token = null;
     const authHeader = req.headers.authorization;
-
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ message: "Missing token" });
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
     }
-
-    const token = authHeader.split(" ")[1];
+    if (!token && req.cookies?.accessToken) {
+      token = req.cookies.accessToken;
+    }
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Bạn chưa đăng nhập",
+      });
+    }
     const decoded = jwt.verify(token, JWT_SECRET);
 
     const user = await prisma.user.findUnique({
       where: { id: decoded.id },
-      select: { id: true, email: true },
+      select: { 
+        id: true, 
+        email: true,  
+        age: true,
+        gender: true,
+        job: true,
+        createdAt: true,
+      },
     });
 
     if (!user) {
-      return res.status(401).json({ message: "User not found" });
+      return res.status(401).json({success: false, message: "User not found" });
     }
-
     req.user = user;
     next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
+  } catch (error) {
+    return res.status(401).json({success: false, message: "Invalid token" });
   }
-};
+}; 
 export default authMiddleware;
