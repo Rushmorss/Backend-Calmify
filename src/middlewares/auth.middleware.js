bcd36244fa9d0c07e1d2f43abccc31ec795c5e17
rushmorss <thangnguyen151004@gmail.com>
2025-12-16 23:05:39 +0700

    sửa lại file authMiddleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key";
 
@@ -21,7 +20,6 @@ const authMiddleware = async (req, res, next) => {
       });
     }
     const decoded = jwt.verify(token, JWT_SECRET);
-
     const user = await prisma.user.findUnique({
       where: { id: decoded.id },
       select: { 
@@ -33,7 +31,6 @@ const authMiddleware = async (req, res, next) => {
         createdAt: true,
       },
     });
-
     if (!user) {
       return res.status(401).json({success: false, message: "User not found" });
     }
